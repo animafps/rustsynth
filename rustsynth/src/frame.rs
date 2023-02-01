0d246bd0b4ae1fd49fb7fc9247ce723170f06371
Anima <18208134+animafps@users.noreply.github.com>
2023-02-02 00:26:48 +1300

    subtypes of nodes and frames groundwork

--- rustsynth/src/frame.rs
+++ rustsynth/src/frame.rs
@@ -2,7 +2,10 @@ use std::{marker::PhantomData, ops::Deref, ptr::NonNull};
 
 use rustsynth_sys as ffi;
 
-use crate::{AudioFormat, VideoFormat};
+use crate::{
+    api::API,
+    format::{AudioFormat, VideoFormat},
+};
 
 /// One frame of a clip.
 // This type is intended to be publicly used only in reference form.
@@ -13,28 +16,43 @@ pub struct Frame<'elem> {
     _owner: PhantomData<&'elem ()>,
 }
 
-/// A reference to a ref-counted frame.
-#[derive(Debug)]
-pub struct FrameRef<'core> {
-    // Only immutable references to this are allowed.
-    frame: Frame<'core>,
+impl<'elem> Drop for Frame<'elem> {
+    fn drop(&mut self) {
+        unsafe { API::get_cached().free_frame(self.handle.as_ptr()) }
+    }
 }
 
 #[derive(Debug)]
-pub struct FrameContext<'core> {
-    _owner: PhantomData<&'core ()>,
+pub struct FrameContext<'elem> {
+    _owner: PhantomData<&'elem ()>,
 }
 
-impl<'core> Frame<'core> {
+impl<'elem> Frame<'elem> {
     pub(crate) fn from_ptr(ptr: *const ffi::VSFrame) -> Self {
         Self {
             handle: unsafe { NonNull::new_unchecked(ptr as *mut ffi::VSFrame) },
             _owner: PhantomData,
         }
     }
+
+    fn ptr(&self) -> *const ffi::VSFrame {
+        self.handle.as_ptr()
+    }
+
+    pub fn get_height(&self, plane: i32) -> i32 {
+        unsafe { API::get_cached().get_frame_height(self.ptr(), plane) }
+    }
+
+    pub fn get_width(&self, plane: i32) -> i32 {
+        unsafe { API::get_cached().get_frame_width(self.ptr(), plane) }
+    }
+
+    pub fn get_length(&self) -> i32 {
+        unsafe { API::get_cached().get_frame_length(self.ptr()) }
+    }
 }
 
-impl<'core> Deref for Frame<'core> {
+impl<'elem> Deref for Frame<'elem> {
     type Target = ffi::VSFrame;
 
     fn deref(&self) -> &Self::Target {
@@ -42,13 +60,38 @@ impl<'core> Deref for Frame<'core> {
     }
 }
 
-#[derive(Debug)]
-pub enum Format<'elem> {
-    Video(VideoFormat<'elem>),
-    Audio(AudioFormat<'elem>),
+impl<'elem> Deref for AudioFrame<'elem> {
+    type Target = Frame<'elem>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<'elem> Deref for VideoFrame<'elem> {
+    type Target = Frame<'elem>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
 }
 
 pub struct AudioFrame<'elem> {
     inner: Frame<'elem>,
-    format: AudioFormat<'elem>,
+    format: AudioFormat,
+}
+
+impl<'elem> AudioFrame<'elem> {
+    pub(crate) fn new(inner: Frame<'elem>, format: AudioFormat) -> Self {
+        Self { inner, format }
+    }
+}
+
+pub struct VideoFrame<'elem> {
+    inner: Frame<'elem>,
+    format: VideoFormat,
+}
+
+impl<'elem> VideoFrame<'elem> {
+    pub(crate) fn new(inner: Frame<'elem>, format: VideoFormat) -> Self {
+        Self { inner, format }
+    }
 }
--- rustsynth/src/node.rs
+++ rustsynth/src/node.rs
@@ -1,7 +1,15 @@
 use rustsynth_sys as ffi;
-use std::{marker::PhantomData, ptr::NonNull};
+use std::{
+    marker::PhantomData,
+    ops::Deref,
+    ptr::{self, NonNull},
+};
 
-use crate::{frame::Frame, prelude::API, MediaType, VideoInfo};
+use crate::{
+    format::{AudioInfo, MediaType, VideoInfo},
+    frame::{AudioFrame, Frame, VideoFrame},
+    prelude::API,
+};
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 pub enum CacheMode {
@@ -10,10 +18,16 @@ pub enum CacheMode {
     ForceDisable,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub struct Node<'elem> {
     handle: NonNull<ffi::VSNode>,
     _elem: PhantomData<&'elem ()>,
+}
+
+impl<'elem> Drop for Node<'elem> {
+    fn drop(&mut self) {
+        unsafe { API::get_cached().free_node(self.handle.as_ptr()) }
+    }
 }
 
 impl<'elem> Node<'elem> {
@@ -24,8 +38,8 @@ impl<'elem> Node<'elem> {
         }
     }
 
-    pub fn get_type(&self) -> MediaType {
-        let result = unsafe { API::get_cached().get_node_type(self.ptr()) };
+    pub(crate) fn get_type(&self) -> MediaType {
+        let result = unsafe { API::get_cached().get_node_type(self.handle.as_ptr()) };
         match result {
             1 => MediaType::Video,
             2 => MediaType::Audio,
@@ -34,31 +48,116 @@ impl<'elem> Node<'elem> {
     }
 
     /// Returns the `VideoInfo` struct if the node is a video node
-    pub fn video_info(&self) -> Option<VideoInfo> {
+    fn video_info(&self) -> Option<VideoInfo> {
         if self.get_type() == MediaType::Audio {
             return None;
         }
-        let info = unsafe { API::get_cached().get_video_info(self.ptr()).read() };
-        Some(info.into())
+        let info = unsafe {
+            API::get_cached()
+                .get_video_info(self.handle.as_ptr())
+                .read()
+        };
+        Some(VideoInfo::from(info))
     }
 
-    pub fn set_cache_mode(&self, mode: CacheMode) {
-        unsafe { API::get_cached().set_cache_mode(self.ptr(), mode as i32) }
+    fn audio_info(&self) -> Option<AudioInfo> {
+        if self.get_type() == MediaType::Video {
+            return None;
+        }
+        let info = unsafe {
+            API::get_cached()
+                .get_audio_info(self.handle.as_ptr())
+                .read()
+        };
+        Some(AudioInfo::from(info))
     }
 
-    pub fn set_cache_options(&self) {
+    pub fn set_cache_mode(&mut self, mode: CacheMode) {
+        unsafe { API::get_cached().set_cache_mode(self.handle.as_ptr(), mode as i32) }
+    }
+
+    pub fn set_cache_options(&mut self) {
         todo!()
     }
 
-    pub(crate) fn ptr(self) -> *mut ffi::VSNode {
+    pub(crate) fn ptr(&self) -> *mut ffi::VSNode {
         self.handle.as_ptr()
     }
 
-    pub fn get_frame(&self, n: i32) -> Option<Frame> {
-        todo!()
+    fn get_frame(&self, n: i32) -> Option<Frame> {
+        let ptr = unsafe {
+            API::get_cached().node_get_frame(self.handle.as_ptr(), n, ptr::null_mut(), 0)
+        };
+        if ptr.is_null() {
+            None
+        } else {
+            Some(Frame::from_ptr(ptr))
+        }
     }
 }
 
-pub struct AudioNode {}
+pub struct AudioNode<'elem> {
+    inner: Node<'elem>,
+}
 
-pub struct VideoNode {}
+impl<'elem> AudioNode<'elem> {
+    pub(crate) fn new(node: Node<'elem>) -> Option<Self> {
+        if node.get_type() == MediaType::Video {
+            None
+        } else {
+            Some(Self { inner: node })
+        }
+    }
+
+    pub fn info(&self) -> AudioInfo {
+        self.inner.audio_info().unwrap()
+    }
+
+    pub fn get_frame(&self, n: i32) -> Option<AudioFrame> {
+        let inner_frame = self.inner.get_frame(n);
+        match inner_frame {
+            Some(frame) => Some(AudioFrame::new(frame, self.info().format)),
+            None => None,
+        }
+    }
+}
+
+impl<'elem> Deref for AudioNode<'elem> {
+    type Target = Node<'elem>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+pub struct VideoNode<'elem> {
+    pub(crate) inner: Node<'elem>,
+}
+
+impl<'elem> VideoNode<'elem> {
+    pub(crate) fn new(node: Node<'elem>) -> Option<Self> {
+        if node.get_type() == MediaType::Audio {
+            None
+        } else {
+            Some(Self { inner: node })
+        }
+    }
+
+    pub fn info(&self) -> VideoInfo {
+        self.inner.video_info().unwrap()
+    }
+
+    pub fn get_frame(&self, n: i32) -> Option<VideoFrame> {
+        let inner_frame = self.inner.get_frame(n);
+        match inner_frame {
+            Some(frame) => Some(VideoFrame::new(frame, self.info().format)),
+            None => None,
+        }
+    }
+}
+
+impl<'elem> Deref for VideoNode<'elem> {
+    type Target = Node<'elem>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
