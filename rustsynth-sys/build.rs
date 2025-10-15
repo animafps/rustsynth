d7fdc4f0d11f337ca63f27a832c39cbafc05d812
Anima <18208134+animafps@users.noreply.github.com>
2025-10-16 12:57:53 +1300

    Enable translation of enum integer types in bindgen configuration for windows translation of cint/cuint in bit enums

--- rustsynth-sys/build.rs
+++ rustsynth-sys/build.rs
@@ -119,6 +119,7 @@ fn main() {
         .rustified_enum("VSTransferCharacteristics")
         .bitfield_enum("VSCoreCreationFlags")
         .bitfield_enum("VSPluginConfigFlags")
+        .translate_enum_integer_types(true)
         .use_core()
         .prepend_enum_name(false)
         .derive_eq(false)
